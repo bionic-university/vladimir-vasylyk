cbf4d258e6c60986656833307ea38f532a82641e
volodymyr Vasylyk <volodymyr.vasylyk@gmail.com>
2014-04-20 17:25:57 +0300

    commit from branch master

--- arr_3.php
+++ arr_3.php
@@ -7,5 +7,10 @@ if ($a == $b) echo "Массивы равны и";
   else echo "Массивы НЕ равны и ";
 if ($a === $b) echo " эквивалентны";
   else echo " НЕ эквивалентны \n";
+
+echo "new change 1 \n";
+
+echo "new change 2, in branch v0.1\n";
 // получим echo "Массивы равны и НЕ эквивалентны"
 ?>
+//new change 1
