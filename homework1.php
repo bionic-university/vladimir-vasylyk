f7790f16b3848997dba27bece815324041342fb8
volodymyr Vasylyk <volodymyr.vasylyk@gmail.com>
2014-04-21 16:22:03 +0300

    working script for counting number of characters in input string

--- homework1.php
+++ homework1.php
@@ -15,26 +15,34 @@ if ($argc < 2) {
 for($k=0; $k < strlen($line);$k++)
 {
     $arr_char[] = $line{$k};
-   // echo $line{$k} . "\n";
-
-    //print($line . "\n");
 }
 $unique_char = array_unique($arr_char);
 $unique_char = array_values($unique_char);
 
 //print_r($arr_char);
 
-print_r($unique_char);
+//print_r($unique_char);
 $counter = 0; //variable to count characters
+$total_char = 0;
 foreach($unique_char as $val){
-    echo "new char " . $val . "\n";
-    for($i=0;$i<$argc;$i++){
+    //echo "Current character " . "{$val}\n";
+    for($i=0;$i<count($arr_char);$i++){
         if ($val == $arr_char[$i]){
             //echo "Symbol!!!";
             $counter++;
-            echo "Counter " . $counter . "\n";
+            //echo "Counter " . $counter . "\n";
         }
     }
     echo "Symbol '{$val}' presented in the input line " . $counter . " times\n";
+    $total_char += $counter;
     $counter=0; //resetting counter
+}
+
+echo "Input string length is  " . strlen($line) . " characters\n";
+echo "Output string length is " . $total_char . " characters\n";
+
+if (strlen($line) == $total_char){
+    echo "Profit! :)\n";
+} else {
+    echo "Oh noooo, not working again\n";
 }
