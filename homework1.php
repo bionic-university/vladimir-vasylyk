22d6fd2c022a972e119dce04f69c04e22ba5d8d8
volodymyr Vasylyk <volodymyr.vasylyk@gmail.com>
2014-04-21 21:16:20 +0300

    new commet to see how it will be merged

--- homework1.php
+++ homework1.php
@@ -1,13 +1,38 @@
 
 <?php
-if ($argc != 2) {
-    echo "Использование: php homework1.php [line of text].\n";
-    exit();
-}
-$line = $argv[1];
+
+echo "Input any string of characters:\n";
+$line = fgets(STDIN);
 
 for($k=0; $k < strlen($line);$k++)
 {
-    echo $line{$k} . "\n";
-    //print($line . "\n");
+    $arr_char[] = $line{$k};
+}
+$unique_char = array_unique($arr_char); //creating array w/ unique values
+$unique_char = array_values($unique_char); //reindexing array w/ unique values
+
+//adding new comment to see how it will be merged
+
+$counter = 0; //variable to count characters
+$total_char = 0;
+
+// for each character in unique array counting number of character in input string
+foreach($unique_char as $val){
+    for($i=0;$i<count($arr_char);$i++){
+        if ($val == $arr_char[$i]){
+            $counter++;
+        }
+    }
+    echo "Symbol '{$val}' presented in the input line " . $counter . " times\n";
+    $total_char += $counter;
+    $counter=0; //resetting counter
+}
+
+echo "Input string length is " . strlen($line) . " characters\n";
+echo "This script processed  " . $total_char . " characters\n";
+
+if (strlen($line) == $total_char){
+    echo "Profit! :)\n";
+} else {
+    echo "Oh noooo, not working again\n";
 }
