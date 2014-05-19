73cb832095af14d88130f44c91f74b05f9d5fa84
volodymyr Vasylyk <volodymyr.vasylyk@gmail.com>
2014-05-19 11:31:50 +0300

    added some phpunit tests

--- src/BionicUniversity/VolodymyrVasylyk/HW3/Translator/Tests/BookTest.php
+++ src/BionicUniversity/VolodymyrVasylyk/HW3/Translator/Tests/BookTest.php
@@ -9,14 +9,14 @@ class BookTest extends \PHPUnit_Framework_TestCase
 {
     public function TestGetTitle()
     {
-        $bookObj = new \Book("SomeAuthor", "SomeTitle"); //Book("SomeAuthor", "SomeTitle");
+        $bookObj = new Book("SomeAuthor", "SomeTitle"); //Book("SomeAuthor", "SomeTitle");
         $someAuthor = "SomeAuthor";
         $this->assertEquals($someAuthor, $bookObj->getTitle());
     }
 
     public function TestSetTitle()
     {
-        $bookObj = new \Book("someAuthor", "someTitle");
+        $bookObj = new Book("someAuthor", "someTitle");
         $otherTitle = "otherTitle";
         $bookObj->setTitle("otherTitle");
         $this->assertEquals($otherTitle, $bookObj->getTitle());
